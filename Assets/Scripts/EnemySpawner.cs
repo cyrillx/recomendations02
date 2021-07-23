9db4ca05609fe264161e03259b8bae3161c85c9a
Kirill <cyrillx@users.noreply.github.com>
2021-07-23 22:31:24 +0300

    Small adjustments

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private Transform _cluster;
+    [SerializeField] private Transform _spawnCluster;
     [SerializeField] private GameObject _enemyTemplate;
 
     private Transform[] _points;
@@ -12,13 +11,11 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        _points = new Transform[_cluster.childCount];
+        _points = new Transform[_spawnCluster.childCount];
 
-        for (int i = 0; i < _cluster.childCount; i++)
+        for (int i = 0; i < _spawnCluster.childCount; i++)
         {
-            _points[i] = _cluster.GetChild(i);
-            Debug.Log("Child " + _cluster.GetChild(i));
-            Debug.Log(_points[i].position.x);
+            _points[i] = _spawnCluster.GetChild(i);
         }
         StartCoroutine(SpawnEnemy());
     }
@@ -27,7 +24,6 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
-            Debug.Log("Dot " + _currentPoint);
             GameObject.Instantiate(_enemyTemplate, _points[_currentPoint].position, Quaternion.identity);
             _currentPoint++;
             if (_currentPoint == _points.Length)
