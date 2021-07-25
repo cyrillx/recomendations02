f9d989474777a91265f0ee01f28f79a9542f7698
Kirill <cyrillx@users.noreply.github.com>
2021-07-25 15:28:52 +0300

    Magic number removal

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform _spawnCluster;
     [SerializeField] private GameObject _enemyTemplate;
+    [SerializeField] private float _spawnDelay;
 
     private Transform[] _points;
     private int _currentPoint;
@@ -28,7 +29,7 @@ public class EnemySpawner : MonoBehaviour
             _currentPoint++;
             if (_currentPoint == _points.Length)
                 _currentPoint = 0;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(_spawnDelay);
         }
     }
 }
